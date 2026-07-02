2d3245e2db8d9b27069432b11f848d928bef324b
Gajanan Shinde <gajananpshinde75@gmail.com>
2026-07-02 23:25:24 +0530

    Learned Seaborn Library

---
+++ Part7_DataVisualization/Part1/line_polt.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+X = [1,2,3,4]
+Y = [5,6,7,8]
+
+plt.plot(X,Y)
+plt.show() # if we are not using notebook if we use python file and end me visualize kare to .show use hota hai
